4c869f58e321b67dff04091c1285b5e209f4718a
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-02-04 13:45:53 +0530

    Changes in review section

---
+++ yummy-slice-pizza/src/components/Reviews/data__reviews.js
@@ -0,0 +1,26 @@
+export const data = [
+  {
+    avatar: AVTR1,
+    name: "Jasmeet Singh",
+    review:
+      "Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec maximus risus sed facilisis       egestas.      Nullam eu bibendum nisl, ut tincidunt metus. Curabitur CurabiturCurabiturmattis sem nisi, vel sollicitudin",
+  },
+  {
+    avatar: AVTR2,
+    name: "Davy Singh",
+    review:
+      "Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec maximus risus sed facilisis       egestas.      Nullam eu bibendum nisl, ut tincidunt metus. Curabitur CurabiturCurabiturmattis sem nisi, vel sollicitudin",
+  },
+  {
+    avatar: AVTR3,
+    name: "Jagmeet Singh",
+    review:
+      "Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec maximus risus sed facilisis       egestas.      Nullam eu bibendum nisl, ut tincidunt metus. Curabitur CurabiturCurabiturmattis sem nisi, vel sollicitudin",
+  },
+  {
+    avatar: AVTR4,
+    name: "Pankaj Kumar",
+    review:
+      "Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec maximus risus sed facilisis       egestas.      Nullam eu bibendum nisl, ut tincidunt metus. Curabitur CurabiturCurabiturmattis sem nisi, vel sollicitudin",
+  },
+];
