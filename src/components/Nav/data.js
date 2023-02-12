b63ed31ab5f17ab9ce47bb8e4745608e81443635
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-02-12 18:09:34 +0530

    changes in links

--- src/components/Nav/data.js
+++ src/components/Nav/data.js
@@ -1,7 +1,7 @@
 export const pageLinks = [
   { id: 1, href: "#home", title: "Home" },
-  { id: 1, href: "#menu", title: "Menu" },
-  { id: 1, href: "#offers", title: "Offers" },
-  { id: 1, href: "#reviews", title: "Reviews" },
-  { id: 1, href: "#contact", title: "Contact" },
+  { id: 2, href: "#menu", title: "Menu" },
+  { id: 3, href: "#offers", title: "Offers" },
+  { id: 4, href: "#reviews", title: "Reviews" },
+  { id: 5, href: "#contact", title: "Contact" },
 ];
